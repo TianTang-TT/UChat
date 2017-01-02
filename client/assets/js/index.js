35f81e5fcd76478fe9f4ccd14123feecfc29192d
heaven <honor_loyalty@126.com>
2017-01-02 19:49:26 +0800

    init socket

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,3 +1,24 @@
 /**
  * created by honor
  */
+//  全局连接变量
+var uchat = null;
+window.onload = function () {
+	uchat = new UChat();
+	uchat.init();
+}
+
+function UChat () {
+	this.socket = null;
+}
+
+UChat.prototype = {
+	init: function () {
+		// 链接到服务器
+		this.socket = io.connect();
+		// 建立连接
+		this.socket.on('connect', function () {
+			// 此处需要展示登陆界面
+		})
+	}
+}
--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const router = require('./router');
+//const router = require('./router');
 
 const app = express();
+const server = http.createServer(app)
+const io = require('socket.io').listen(server);
+
 app.set('view engine', 'jade'); // 设置模板引擎
 app.set('views', __dirname + '/static/tpls');  // 设置模板相对路径(相对当前目录)
 app.use(express.static(path.resolve(__dirname, '../client')));
@@ -12,6 +16,12 @@ app.use((req, res, next) => {
   next();
 });
 
-router(app);
+// router(server);
 
-app.listen(7777, () => {console.log('server is started!')});
+server.listen(7777, () => {console.log('server is started!')});
+
+io.on('connection', socket => {
+	socket.on('foo', data => {
+		console.log(data);
+	})
+})
