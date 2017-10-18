22c1bbbeccd1c92829a9364ef2878852398fbac7
honor <honor_loyalty@126.com>
2017-10-18 17:34:33 +0800

    show or hide emoji  tab

---
+++ client/src/view/main/content/chats/contentedit/Emoji.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="emoji-container">
+    <div class="tab">
+      <section class="emoji-list small" data-type="qq">
+        <img src="~assets/img/qq/1.gif" data-type="qq" data-num="1">
+        <img src="~assets/img/qq/1.gif" data-type="qq" data-num="1">
+        <img src="~assets/img/qq/1.gif" data-type="qq" data-num="1">
+      </section>
+      <section class="emoji-list common" data-type="tsj">
+        <img src="~assets/img/tsj/1.gif" data-type="tsj" data-num="1">
+      </section>
+    </div>
+    <ul class="selector clearfix">
+      <li class="selector-li" data-type="qq"><img src="~assets/img/qq/1.gif"></li>
+      <li class="selector-li" data-type="tsj"><img src="~assets/img/tsj/1.gif"></li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'emoji',
+    props: {
+      hideHandler: {
+        type: Function,
+        required: true
+      }
+    },
+    methods: {
+      hideTab (e = window.event) {
+        if (!this.$el.contains(e.target)) {
+          this.hideHandler()
+        }
+      }
+    },
+    created () {
+      document.addEventListener('click', this.hideTab)
+    },
+    destroyed () {
+      document.removeEventListener('click', this.hideTab)
+    }
+  }
+</script>
+<style lang="less" scoped="">
+  .emoji-container {
+    width: 460px;
+    height: 290px;
+    box-sizing: border-box;
+    position: absolute;
+    top: -290px;
+    left: -100px;
+    background-color: #fff;
+    border: 1px solid #dadada;
+    border-radius: 2px;
+    .tab {
+      height: 250px;
+      .emoji-list {
+        width: 100%;
+        height: 250px;
+        box-sizing: border-box;
+        padding: 10px;
+        position: absolute;
+        top: 0;
+        bottom: 40px;
+        overflow: auto;
+        img:hover {
+          background-color: #dcdcdc;
+        }
+      }
+      .common {
+        display: none;
+        img {
+          width: 50px;
+          height: 50px;
+          margin: 0 4px;
+        }
+      }
+      .small {
+        img {
+          width: 24px;
+          height: 24px;
+          margin: 0 4px;
+        }
+      }
+    }
+
+    .selector {
+      height: 40px;
+      width: 100%;
+      padding: 0 15px;
+      position: absolute;
+      bottom: 0;
+      border-top: 1px solid #dadada;
+      box-sizing: border-box;
+      .selector-li {
+        float: left;
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        img {
+          width: 24px;
+          height: 24px;
+          vertical-align: middle;
+        }
+      }
+      [data-type=tsj] img {
+        width: 26px;
+        height: 26px;
+      }
+    }
+  }
+</style>
--- client/src/view/main/content/chats/contentedit/index.vue
+++ client/src/view/main/content/chats/contentedit/index.vue
@@ -2,25 +2,11 @@
   <section class="contentedit">
     <!--聊天工具-->
     <ul class="tools clearfix">
-      <li class="tool-item emoji active"><i class="iconfont icon-face"></i>
-        <div class="emoji-container">
-          <div class="tab">
-            <section class="emoji-list small" data-type="qq">
-              <img src="~assets/img/qq/1.gif" data-type="qq" data-num="1">
-              <img src="~assets/img/qq/1.gif" data-type="qq" data-num="1">
-              <img src="~assets/img/qq/1.gif" data-type="qq" data-num="1">
-            </section>
-            <section class="emoji-list common" data-type="tsj">
-              <img src="~assets/img/tsj/1.gif" data-type="tsj" data-num="1">
-            </section>
-          </div>
-          <ul class="selector clearfix">
-            <li class="selector-li" data-type="qq"><img src="~assets/img/qq/1.gif"></li>
-            <li class="selector-li" data-type="tsj"><img src="~assets/img/tsj/1.gif"></li>
-          </ul>
-        </div>
+      <li class="tool-item emoji active" @click.stop="showEmojiTab">
+        <i class="iconfont icon-face"></i>
+        <emoji v-if="emojiVisible" :hideHandler="hideEmojiTab"></emoji>
       </li>
-      <li class="tool-item printscreen" style="display: none">
+      <li class="tool-item printscreen">
         <i class="iconfont icon-scissors"></i>
       </li>
       <li class="tool-item upload">
@@ -42,8 +28,12 @@
 </template>
 <script>
   import { mapActions, mapState } from 'vuex'
+  import Emoji from './Emoji'
   export default {
     name: 'contentedit',
+    components: {
+      Emoji
+    },
     props: {
       chatInfo: {
         type: Object,
@@ -52,7 +42,7 @@
     },
     data () {
       return {
-        edittingMsg: ''
+        emojiVisible: false
       }
     },
     computed: {
@@ -76,6 +66,12 @@
           }
         })
         this.$refs['typeContent'].innerHTML = ''
+      },
+      showEmojiTab () {
+        this.emojiVisible = true
+      },
+      hideEmojiTab () {
+        this.emojiVisible = false
       }
     }
   }
@@ -101,75 +97,6 @@
       }
       .emoji {
         position: relative;
-        .emoji-container {
-          width: 460px;
-          height: 290px;
-          box-sizing: border-box;
-          position: absolute;
-          top: -290px;
-          left: -100px;
-          background-color: #fff;
-          border: 1px solid #dadada;
-          border-radius: 2px;
-          display: none;
-          .tab {
-            height: 250px;
-            .emoji-list {
-              width: 100%;
-              height: 250px;
-              box-sizing: border-box;
-              padding: 10px;
-              position: absolute;
-              top: 0;
-              bottom: 40px;
-              overflow: auto;
-              img:hover {
-                background-color: #dcdcdc;
-              }
-            }
-            .common {
-              display: none;
-              img {
-                width: 50px;
-                height: 50px;
-                margin: 0 4px;
-              }
-            }
-            .small {
-              img {
-                width: 24px;
-                height: 24px;
-                margin: 0 4px;
-              }
-            }
-          }
-
-          .selector {
-            height: 40px;
-            width: 100%;
-            padding: 0 15px;
-            position: absolute;
-            bottom: 0;
-            border-top: 1px solid #dadada;
-            box-sizing: border-box;
-            .selector-li {
-              float: left;
-              width: 40px;
-              height: 40px;
-              line-height: 40px;
-              text-align: center;
-              img {
-                width: 24px;
-                height: 24px;
-                vertical-align: middle;
-              }
-            }
-            [data-type=tsj] img {
-              width: 26px;
-              height: 26px;
-            }
-          }
-        }
       }
       .upload {
         .icon-folder {
