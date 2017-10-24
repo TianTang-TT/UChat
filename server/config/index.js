754e24d8f67ec64e0834b555aa15dd06b8cf081f
honor <honor_loyalty@126.com>
2017-10-24 15:07:02 +0800

    add config and middlewares

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.export = {
+  dev: {
+    port: 3001
+  },
+  prod: {
+    port: 3002
+  }
+}
---
+++ server/middlewares/response_formatter.js
@@ -0,0 +1,15 @@
+/**
+ *  format response
+ */
+
+module.export.formatResponse = async (ctx, next) => {
+  // 先执行路由，等回溯的时候对结果进行格式化
+  await next()
+
+  // 将执行结果转为固定的格式
+  ctx.body = {
+    code: 200,
+    message: 'success',
+    data: ctx.body
+  }
+}
