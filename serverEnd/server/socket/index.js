3d0c42f6262b42fc1a6005afd6a3d774f6aa7eb8
honor <honor_loyalty@126.com>
2017-11-01 16:51:37 +0800

    add login event for socket

--- serverEnd/server/socket/index.js
+++ serverEnd/server/socket/index.js
@@ -2,8 +2,14 @@
  * 初始化socket
  * @param socket
  */
+// 当前在线人员
+const onlineNumbers = []
+
+// 群聊，包括私聊和群聊
+const chatGroup = {}
+
 module.exports = socketIO => {
   socketIO.on('connection', socket => {
-    socket.emit('news', {hello: 'world'})
+    socket.on('login', user => onlineNumbers.push(user))
   })
 }
