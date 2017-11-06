41ff0db0af704c2aa076343eaf048fb8c3412a88
honor <honor_loyalty@126.com>
2017-11-06 18:01:09 +0800

    add babel-plugin-component to use some component

--- clientEnd/src/main.js
+++ clientEnd/src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import { Message, MessageBox, Notification } from 'element-ui'
+import { Button, Message, MessageBox, Notification } from 'element-ui'
 
 import 'element-ui/lib/theme-chalk/index.css'
 import 'static/css/reset.css'
 import 'assets/css/iconfont.css'
 
 Vue.config.productionTip = false
+Vue.use(Button)
 Vue.use(Message)
 Vue.use(MessageBox)
 Vue.use(Notification)
