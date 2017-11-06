70e223e8bf940d31059feff95bea508735144253
honor <honor_loyalty@126.com>
2017-11-06 17:45:21 +0800

    init element-ui

--- clientEnd/src/main.js
+++ clientEnd/src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import 'components'  // 引入全局组件
+import { Message, MessageBox, Notification } from 'element-ui'
 
+import 'element-ui/lib/theme-chalk/index.css'
 import 'static/css/reset.css'
 import 'assets/css/iconfont.css'
 
 Vue.config.productionTip = false
+Vue.use(Message)
+Vue.use(MessageBox)
+Vue.use(Notification)
 
 /* eslint-disable no-new */
 new Vue({
