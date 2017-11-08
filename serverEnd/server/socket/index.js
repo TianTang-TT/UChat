8563e7fa16877e9a96f421c573f801b11f76a79c
honor <honor_loyalty@126.com>
2017-11-08 15:43:21 +0800

    deal start chat request

--- serverEnd/server/socket/index.js
+++ serverEnd/server/socket/index.js
@@ -42,6 +42,18 @@ module.exports = socketIO => {
       socket.broadcast.emit('online', onlineNumbers.get(id))
     })
 
+    // 发起聊天
+    socket.on('startChat', contact => {
+      // 检查contact是否在线，并且不是自己跟自己聊天
+      if (socket.id === contact.id) {
+        console.log('............有个逗比要跟自己聊天..............')
+      } else if (!onlineNumbers.get(contact.id)) {
+        console.log('............你要撩的人不在线...........')
+      } else {
+        // 创建房间，开始对话
+      }
+    })
+
     // 接受消息事件
     socket.on('message', msg => {
 
